7cf6943d5295989cc23e9a8f97649e3cc9d38fb9
Kirill Katz <xnlyre@protonmail.com>
2025-08-07 21:08:39 +0300

    Add C/C++ debugger

---
+++ lua/config/lazy/dap.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+      require("nvim-dap-virtual-text").setup()
+
+      dap.adapters.cppdbg = {
+          id = 'cppdbg',
+          type = 'executable',
+          command = vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7",
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch with args",
+          type = "cppdbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          args = function()
+            local input = vim.fn.input("Program arguments: ")
+            return vim.split(input, " ", { trimempty = true })
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = true,
+          setupCommands = {
+            {
+              text = "-enable-pretty-printing",
+              description = "enable pretty printing",
+              ignoreFailures = true
+            },
+          },
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F6>", dap.restart)
+      vim.keymap.set("n", "<F12>", function()
+        require("dap").terminate()
+        require("dapui").close()
+      end, { desc = "Stop debugger" })
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
+}

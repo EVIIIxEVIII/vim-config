69d2ce5d7ad031c975627793d2095c604b87717e
Kirill Katz <xnlyre@protonmail.com>
2025-08-08 10:22:06 +0300

    Add relative number lines in the explorer

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -3,6 +3,14 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        vim.opt_local.number = true
+        vim.opt_local.relativenumber = true
+    end,
+})
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
